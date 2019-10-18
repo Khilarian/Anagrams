c234cdc193016fba3b55a58daf3f31a6621ba8d8
Kirill Rumakin <donnorvator@gmail.com>
2019-10-18 17:22:34 +0300

    Rewrite makeAnagramWord()

--- src/com/foxminded/anagram/AppStarter.java
+++ src/com/foxminded/anagram/AppStarter.java
@@ -3,7 +3,7 @@ package com.foxminded.anagram;
 public class AppStarter {
 	
 	public static void main(String [] args) {
-		String text = "hello, my brother from 2k19!";
+		String text = "h1111111ello , what1aa";
 		LetterAnagram anagram = new LetterAnagram();		
 		System.out.println(anagram.makeAnagram(text));
 	}
--- src/com/foxminded/anagram/LetterAnagram.java
+++ src/com/foxminded/anagram/LetterAnagram.java
@@ -1,54 +1,44 @@
 package com.foxminded.anagram;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class LetterAnagram {
 	
-	public String makeAnagram(String text) {
-		String[] words = text.split(" +");
-		String[] result = new String[words.length];	
-		for (int i = 0; i < words.length; i++) {
-			result[i] = makeWordAnagram(words[i]);
-		}	
-		return String.join(" ",result);
-	}
-	
-	private String makeWordAnagram(String word) {
-		StringBuilder sb = new StringBuilder();
-		int indexLastLetter = word.length() - 1;
-		
-		for (int i = 0; i < word.length(); i++) {
-		    
-			char currentChar = word.charAt(i);
-			if (!Character.isLetter(currentChar)) {			
-				sb.append(currentChar);
-			} else {
-				char lastLetterChar = word.charAt(indexLastLetter);
-				if (Character.isLetter(lastLetterChar)) {   		
-					sb.append(word.charAt(indexLastLetter));
-					indexLastLetter--;
-				} else {
-					indexLastLetter = previousLetterIndex(word, indexLastLetter);					 
-					sb.append(word.charAt(indexLastLetter));
-					indexLastLetter--;
-				}			
-			}
-		}
-		return sb.toString();
-	}
+    public String makeAnagram(String text) {
+        String [] words = text.split(" +");
+        String [] result = new String [words.length];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = makeWordAnagram(words[i]);
+        }	
+        return String.join(" ", result);
+    }
 	
 
-	private int previousLetterIndex(String word, int index) {
-		int returnIndex = 0;
-		for (int i = index; i >= 0; i--) {
-			if (Character.isLetter(word.charAt(i)))  {
-				returnIndex = i;
-				break;
-			}
-		}
-		return returnIndex;
-	}
+    private String makeWordAnagram(String word) {
+        StringBuilder sb = new StringBuilder(word);
+        int indexLastLetter = word.length() - 1;
+        for(int i = 0; i < sb.length() && i < indexLastLetter; i++) {
+            if (Character.isLetter(word.charAt(i))) {
+                swap(sb, i, previousLetterIndex(word, indexLastLetter));
+                indexLastLetter--;
+            }
+        }
+        return sb.toString();
+    }
+    
+    private void swap(StringBuilder word, int first, int second) {
+        char temp = word.charAt(first);
+        word.setCharAt(first, word.charAt(second));
+        word.setCharAt(second, temp);    
+    }
+    
+    private int previousLetterIndex(String word, int index) {
+        for (int i = index; i >= 0; i--) {
+            if (Character.isLetter(word.charAt(i)))  {
+                return i;	
+            }
+        }
+        return -1;
+    }
 }
     
+
  
