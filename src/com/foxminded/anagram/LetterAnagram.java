75e09bbc44b23e5ad1c0331ec6233c78e6f372f2
Kirill Rumakin <donnorvator@gmail.com>
2019-10-22 10:56:22 +0300

    Rewrite makeWordAnagram():
    now j is not just decremented, it calculated with previousLetterIndex();

--- src/com/foxminded/anagram/LetterAnagram.java
+++ src/com/foxminded/anagram/LetterAnagram.java
@@ -1,22 +1,22 @@
 package com.foxminded.anagram;
 
 public class LetterAnagram {
-	
+    
     public String makeAnagram(String text) {
-        String [] words = text.split(" +");
-        String [] result = new String [words.length];
+        String[] words = text.split(" +");
+        String[] result = new String[words.length];
         for (int i = 0; i < result.length; i++) {
-            result [i] = makeWordAnagram(words[i]);
-        }	
+            result[i] = makeWordAnagram(words[i]);
+        }
         return String.join(" ", result);
     }
-	
+    
     private String makeWordAnagram(String inputWord) {
         char[] word = inputWord.toCharArray();
-        for(int i = 0, j = inputWord.length() -1; i < word.length && i < j; i++) {
+        for (int i = 0, j = inputWord.length(); i < word.length && i < j; i++) {
             if (Character.isLetter(inputWord.charAt(i))) {
-                swap(word, i, previousLetterIndex(inputWord, j));
-                j--;
+                j = previousLetterIndex(inputWord, j);
+                swap(word, i, j);
             }
         }
         return new String(word);
@@ -25,16 +25,15 @@ public class LetterAnagram {
     private void swap(char[] word, int first, int second) {
         char temp = word[first];
         word[first] = word[second];
-        word[second] = temp;;    
+        word[second] = temp;
     }
     
     private int previousLetterIndex(String word, int index) {
-        for (int i = index; i >= 0; i--) {
-            if (Character.isLetter(word.charAt(i)))  {
-                return i;	
+        for (int i = index - 1; i >= 0; i--) {
+            if (Character.isLetter(word.charAt(i))) {
+                return i;
             }
         }
         throw new IllegalArgumentException("No more letter in word");
     }
 }
-    
